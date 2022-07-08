99b801e0013e729c752ec4c884af25ddce7642fb
Achel <rachel1665@iCloud.com>
2022-07-08 16:29:42 +0800

    达到预期效果

--- src/main.js
+++ src/main.js
@@ -1,8 +1,42 @@
 let html = document.querySelector("#html")
 let style = document.querySelector("#style")
-let string = `测试测试测试测试测试测试测试测试
+let string = `/*测试测试测试测试测试测试测试测试
 测试   测试测试
 测试测试测试
+**/
+#div1 {
+    border: 1px solid red;
+    width: 200px;
+    height: 200px;
+}
+#div1 {
+    border-radius: 50%;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    border: none;
+}
+#div1 {
+    background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);
+}
+#div1::before {
+    width: 100px;
+    height: 100px;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    background: #000;
+    border-radius: 50%;
+    background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(0,0,0,1) 25%, rgba(0,0,0,1) 100%);
+}
+#div1::after {
+    width: 100px;
+    height: 100px;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    background: #fff;
+    border-radius: 50%;
+    background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 100%, rgba(0,0,0,1) 100%);
+}
 `
 let string2 = ""
 let n = 0
@@ -17,8 +51,11 @@ let step = () => {
             string2 += string[n]
         }
         html.innerHTML = string2
+        style.innerHTML = string.substring(0, n)
+        window.scrollTo(0,document.body.scrollHeight)
+        html.scrollTo(0,document.body.scrollHeight)
         if (n < string.length - 1) {
-            n++
+            n += 1
             step()
         }
     }, 10)
